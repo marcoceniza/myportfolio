9ac95e0de56703f66102ec259f5de8de407fe298
Marco Ceniza <marcoceniza15@gmil.com>
2021-06-22 17:17:33 +0800

    Edited

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  let webWith = $(window).width();
+
+  $(window).resize(function() {
+
+    if(webWith <= 575) {
+      $('nav').addClass('bg-dark');
+    } else {
+      $('nav').removeClass('bg-dark');
+    }
+    
+  })
+
 });
